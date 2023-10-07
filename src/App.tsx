ce402ad79032e29bbe1805393e82ea7830260198
Roger Enand <rogerenand@gmail.com>
2023-10-06 20:37:15 -0500

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-import { Event, ModeNight, WbSunny } from '@mui/icons-material';
+import { ModeNight, WbSunny } from '@mui/icons-material';
 import EventEditor from '@/pages/eventEditor';
 import { ThemeProvider } from '@emotion/react';
-import { CssBaseline, Fab, useTheme } from '@mui/material';
+import { AppBar, CssBaseline, IconButton, Toolbar } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import { useState } from 'react';
-import "./app.scss"
-import UserProfile from './components/UserProfile/UserProfile';
+import './app.scss';
 
 function Copyright() {
 	return (
-		<Typography variant="body2" color="text.secondary" align="center">
+		<Typography variant='body2' color='text.secondary' align='center'>
 			{'Copyright © '}
-			<Link color="inherit" href="https://mui.com/">
+			<Link color='inherit' href='https://mui.com/'>
 				Your Website
 			</Link>{' '}
 			{new Date().getFullYear()}.
@@ -26,27 +23,37 @@ function Copyright() {
 }
 
 export default function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
-  const theme = createTheme({
-    palette: {
-      mode: isDarkTheme ? 'dark' : 'light',
-    },
-  });
-  const themeIcon = () => {
-    if (isDarkTheme) return <WbSunny />
-    return <ModeNight/>
-  }
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box sx={{ my: 4,mx:8 }}>
-        <EventEditor/>
-      </Box>
-        <Fab className={'fab'} size="small" aria-label="add" onClick={() => {
-          setIsDarkTheme(!isDarkTheme)
-        }}>
-          {!isDarkTheme ? <WbSunny /> : <ModeNight/>}
-        </Fab>
-    </ThemeProvider>
-  );
+	const [isDarkTheme, setIsDarkTheme] = useState(true);
+	const theme = createTheme({
+		palette: {
+			mode: isDarkTheme ? 'dark' : 'light',
+		},
+	});
+	const themeIcon = () => {
+		if (isDarkTheme) return <WbSunny />;
+		return <ModeNight />;
+	};
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position='static'>
+					<Toolbar>
+						<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+							Hackathon App Name Here
+						</Typography>
+						<IconButton onClick={() => {
+							setIsDarkTheme(!isDarkTheme);
+						}}>
+							{!isDarkTheme ? <WbSunny /> : <ModeNight />}
+						</IconButton>
+
+					</Toolbar>
+				</AppBar>
+			</Box>
+			<Box sx={{ my: 4, mx: 8 }}>
+				<EventEditor />
+			</Box>
+		</ThemeProvider>
+	);
 }
