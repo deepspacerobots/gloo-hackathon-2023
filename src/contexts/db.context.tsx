79b61efe939612d34e494fa71fffb4282a1c3f24
Andrew Hale <services.hale.adh@gmail.com>
2023-10-06 20:05:08 -0400

    Add db context

--- src/contexts/db.context.tsx
+++ src/contexts/db.context.tsx
@@ -1,9 +1,19 @@
 import { Database, preexistingData } from '@/db/db';
-import { createContext } from 'react';
+import { ReactNode, createContext, useContext } from 'react';
+
+type Props = {
+	children: string | ReactNode | JSX.Element;
+};
 
 const database = new Database(preexistingData);
 type DBContextType = Database;
 
 const DBContext = createContext<DBContextType>(database);
 
-const CompanyProvider = ({ children }: Props): JSX.Element => {};
+const DBProvider = ({ children }: Props): JSX.Element => {
+	return <DBContext.Provider value={database}>{children}</DBContext.Provider>;
+};
+
+export default DBProvider;
+
+export const useDBContext = () => useContext<DBContextType>(DBContext);
