f60018e58785f5148b8180906c7d09524c78331c
Roger Enand <rogerenand@gmail.com>
2023-10-07 10:24:11 -0500

    removed extra themeing

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
 import App from './App';
 import DBProvider from './contexts/db.context';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<DBProvider>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<App />
-			</ThemeProvider>
+			<App />
 		</DBProvider>
-	</React.StrictMode>
+	</React.StrictMode>,
 );
